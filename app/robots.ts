a070a3cef9e85048807d10a438a52a127e3bd226
Parth Bhuptani <240173132002@vgecg.ac.in>
2026-08-11 17:13:24 +0530

    Update base URL in robots function

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,10 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 import { site } from "@/config/site";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = site.url || "https://parthbhuptani.me";
+  const baseUrl =
+    site.url || "https://parthbhuptani.vercel.app";
+
   return {
     rules: {
       userAgent: "*",
